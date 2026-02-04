2ebc9917b90fd07a419d2fbc4713de044f9f6453
Juri Ohto <wsb.1099@gmail.com>
2026-02-04 23:42:49 +0900

    ログイン状態を監視

--- lib/features/recipe/presentation/providers/recipe_providers.dart
+++ lib/features/recipe/presentation/providers/recipe_providers.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:my_recipe_memo/features/auth/presentation/providers/auth_providers.dart';
 import 'package:my_recipe_memo/features/recipe/data/recipe_repository.dart';
 import 'package:my_recipe_memo/features/recipe/models/recipe.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -8,5 +9,12 @@ part 'recipe_providers.g.dart';
 
 @riverpod
 Stream<List<Recipe>> recipes(Ref ref) {
+  // ユーザーの変更を監視して再構築する
+  final user = ref.watch(authStateProvider).value;
+
+  if (user == null) {
+    return Stream.value([]);
+  }
+
   return ref.watch(recipeRepositoryProvider).watchRecipes();
 }
