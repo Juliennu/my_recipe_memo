9c0798b2850451d652c029c85972ed4988848792
Juri Ohto <wsb.1099@gmail.com>
2026-02-05 00:13:44 +0900

    ステータスバーの視認性向上

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:my_recipe_memo/core/router/router.dart';
@@ -41,6 +42,7 @@ class MyApp extends ConsumerWidget {
         appBarTheme: const AppBarTheme(
           centerTitle: true,
           backgroundColor: Colors.transparent,
+          systemOverlayStyle: SystemUiOverlayStyle.dark,
           elevation: 0,
           titleTextStyle: TextStyle(
             color: AppColors.text,
