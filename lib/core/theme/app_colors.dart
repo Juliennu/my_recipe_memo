344cefb5f2a684015e122247fbb7781d19faabfb
Juri Ohto <wsb.1099@gmail.com>
2026-02-04 18:02:25 +0900

    add AppColors

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color primary = Color(0xFFD48B8B); // くすみピンク
+  static const Color surface = Color(0xFFFFF7F5); // ほんのりピンクがかった白
+  static const Color secondary = Color(0xFFBCAAA4);
+  static const Color text = Color(0xFF5D4037);
+  static const Color textSecondary = Color(0xFF8D6E63);
+
+  // コンポーネント用
+  static const Color inputBorder = Color(0xFFD48B8B);
+  static const Color hintText = Color(0xFFBDBDBD); // Colors.grey[400]の近似
+}
